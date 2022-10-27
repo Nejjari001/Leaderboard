ea2a78341a449a061cca71af501f654314206faf
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2022-10-27 09:33:16 +0100

    Update displayData.js module file by adding fetch data function

--- src/modules/displayData.js
+++ src/modules/displayData.js
@@ -1,4 +1,5 @@
 const display = document.querySelector('.names-scores');
+const id = localStorage.getItem('game');
 
 const displayData = (LeaderData) => {
   if (!LeaderData.length) display.innerHTML = '<p class=\'details\'>No scores available!</p>';
@@ -11,5 +12,16 @@ const displayData = (LeaderData) => {
     nameUser.innerHTML = `${data.name} : ${data.score}`;
   });
 };
+
+const fetchScoresFromApi = async () => {
+  if (id) {
+    const fetchUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`;
+    const response = await fetch(fetchUrl);
+    const fetchedData = await response.json();
+    displayData(fetchedData.result);
+  }
+};
+
+fetchScoresFromApi();
 
 export default displayData;
