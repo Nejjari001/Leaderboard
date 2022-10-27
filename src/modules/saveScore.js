9acd4f2be15b79e56b8fde57abd7758b5c910ea8
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2022-10-27 09:58:32 +0100

    Fix linters/ESlint  Errors

--- src/modules/saveScore.js
+++ src/modules/saveScore.js
@@ -6,7 +6,7 @@ const score = document.getElementById('score');
 const id = localStorage.getItem('game');
 
 const saveScore = async () => {
-  const data = new Score(name.value, score.value);
+  const data = new ScoreResult(name.value, score.value);
   const saveUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`;
 
   const params = {
